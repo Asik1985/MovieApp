cdf2d66891bdcf910954f662fee6a4d57ba876c5
Asik1985 <assylbekezhanuly@yahoo.com>
2023-03-17 20:57:55 +0600

    Movie

---
+++ MovieApp/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieApp.Models;
+
+public class Movie
+{
+    [Key]
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public int Price { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+
+    public int CategoryId { get; set; }
+    public Category? Category { get; set; }
+}
